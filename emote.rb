700c6d550870848f7bf51e86a8e052844247c570
richard trapanese <richardtrapanese@gmail.com>
2018-08-28 15:28:55 -0400

    created a an emoji class that has functions that return different states

---
+++ emote.rb
@@ -0,0 +1,36 @@
+class Emoji_State
+    def initialize(str)
+        @str = str
+    end
+    def original()
+        @str
+    end
+    def sad()
+        
+       @str + "😢"
+    end
+    def happy()
+        
+        @str +  "🤪"
+    end
+    def laughing()
+        @str + "🤣"
+    end
+    def angry()
+        @str +  "🤬"
+
+    end
+end
+sentence = Emoji_State.new("I actually code!")
+puts sentence.original()
+puts sentence.sad()
+puts sentence.happy()
+puts sentence.laughing()
+puts sentence.angry()
+
+sentence1 = Emoji_State.new("Sup' Groot?")
+puts sentence1.original()
+puts sentence1.sad()
+puts sentence1.happy()
+puts sentence1.laughing()
+puts sentence1.angry()
