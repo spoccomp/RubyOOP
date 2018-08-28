6a9c950ee538474f6fe18e12b65050593ab4b246
richard trapanese <richardtrapanese@gmail.com>
2018-08-28 13:35:58 -0400

    created new class Calculator

--- animal.rb
+++ animal.rb
@@ -3,35 +3,22 @@ attr_reader  :height, :species
 # attr_writer
 attr_accessor :name
 
-    def initialize(name, height, species)
-        @name = name
-        @height = height
-        @species = species
-    end 
+		def initialize(name, height, species)
+				@name = name
+				@height = height
+				@species = species
+		end 
 
-    def walk()
-        puts "I am walking very slowly"
-    end
+		def walk()
+				puts "I am walking very slowly"
+		end
 
-    def run()
-        puts "I am running very quickly"
-    end
+		def run()
+				puts "I am running very quickly"
+		end
 
 end
-class Calculator
-    def self.add(arg1, arg2)
-        arg1 + arg2
-    end
-    def self.subtract(arg1, arg2)
-        arg1 - arg2
-    end
-    def self.product(arg1, arg2)
-        arg1 * arg2
-    end
-    def self.divide(arg1, arg2)
-        arg1 / arg2
-    end
-end
+
 
 giraffe = Animal.new("buddy",20,"mammal")
 lizard = Animal.new("snakey", 4, "reptile")
---
+++ calculator.rb
@@ -0,0 +1,14 @@
+class Calculator
+    def self.add(arg1, arg2)
+            arg1 + arg2
+    end
+    def self.subtract(arg1, arg2)
+            arg1 - arg2
+    end
+    def self.product(arg1, arg2)
+            arg1 * arg2
+    end
+    def self.divide(arg1, arg2)
+            arg1 / arg2
+    end
+end
